027879c5a98f427f2a46d0e47274c3b40e316372
backpack5689 <aaron.thompson1889@outlook.com>
2020-07-22 18:04:21 -0500

    Added more stopwatch functionality, like bump control and weird start issues

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 var timer = document.getElementById("timerDisplay");
 var watch = new stopwatch(timer);
-var halt = 1;
+var halt = 0;
+var keyDown = false;
+var hold = 0;
+var threshold = 25;
 document.body.addEventListener('keydown', function(key) {
     if(key.which === 32){
-        if(watch.isOn){
-            watch.stop();
-            halt = 1;
+        hold++;
+        console.log(hold);
+        if (!keyDown) {
+            keyDown = true;
+            if(watch.isOn){
+                watch.stop();
+                halt = 1;
+            }
         }
-        else{
+        else if(hold >= threshold){
             watch.reset();
         }
     }
 });
 document.body.addEventListener('keyup', function(key){
     if(key.which === 32){
-        if(halt === 0){
+        console.log(hold);
+        if(halt === 0 && hold >= threshold){
             watch.start();
         }
         else{
+            console.log("Removing value for hold");
             halt = 0;
+            hold = 0;
         }
+        keyDown = false;
     }
-})
+});
