4ded91079880d52a3a11e4659ca4b725d21ede79
backpack5689 <aaron.thompson1889@outlook.com>
2020-07-22 15:03:12 -0500

    Completed the beginnings of the Stopwatch with start/stop functionality bound to the spacebar

--- timer_script.js
+++ timer_script.js
@@ -1,64 +1,62 @@
 //Using this tutorial: <https://youtu.be/jRhB1IG7uAw>
-function stopwatch(){
+function stopwatch(elem){
+    //Private Variables
     var time = 0;
     var interval;
     var offset;
+    
+    //Public Variables
     this.isOn = false;
 
-    function update(){
-        time += delta();
-        var formattedTime = timeFormatter(time);
-    };
-    function delta(){
+    //Private Functions
+    function update() {
+        if(this.isOn){
+            time += delta();
+        }
+        var formattedTime = format(time);
+        elem.textContent = formattedTime;
+    }
+    function delta() {
         var now = Date.now();
         var timePassed = now - offset;
         offset = now;
-        return timePassed;
-    };
-    function timeFormatter(timems){
-        var t = new Date(timems);
-        var minutes = t.getMinutes().toString();
-        var seconds = t.getSeconds().toString();
-        var mili = t.getMilliseconds().toString();
-
-        if(minutes.length < 2)
-        {
-            minutes = '0'  + minutes;
+        return timePassed
+    }
+    function format(rawTime) {
+        var dTime = new Date(rawTime);
+        var minutes = dTime.getMinutes().toString();
+        var seconds = dTime.getSeconds().toString();
+        var milliseconds = dTime.getMilliseconds().toString();
+        if(minutes.length < 2){
+            minutes = '0' + minutes;
         }
-        if(seconds.length < 2)
-        {
-            seconds = '0'  + seconds;
+        if(seconds.length < 2){
+            seconds = '0' + seconds;
         }
-        while(mili.length < 3)
+        while(milliseconds.length < 3)
         {
-            mili = '0' + mili;
+            milliseconds = '0' + milliseconds;
         }
+        return minutes + " : " + seconds + " . " + milliseconds;
+    }
 
-        return minutes + " : " + seconds + " . " + mili;
-    };
-
-
-    this.start = function(){
+    //Public Functions
+    this.start = function() {
         if(!this.isOn){
-            interval = setInterval(update, 10);
+            interval = setInterval(update.bind(this), 25);
             offset = Date.now();
             this.isOn = true;
         }
     };
-
-    this.stop = function(){
+    this.stop = function() {
         if(this.isOn){
             clearInterval(interval);
             interval = null;
-            this.isOn = false;
+            this.isOn = null;
         }
     };
-
-    this.reset = function()
-    {
+    this.reset = function() {
         time = 0;
         update();
-    }
+    };
 }
-
-var watch = new stopwatch();
