92a7afa5c09f4a14eb3f950e031f406794bd2265
backpack5689 <aaron.thompson1889@outlook.com>
2020-07-21 20:44:27 -0500

    doing things with things

--- timer_script.js
+++ timer_script.js
@@ -1,2 +1,21 @@
+//Define vars to hold time data
+let ms = 0;
+let s = 0;
+let m = 0;
+
+//Define timer function
+function stopwatch(){
+    ms++;
+    if(ms / 100===1){
+        ms = 0;
+        s++;
+    }
+    if(s / 60 === 1){
+        s = 0;
+        m++;
+    }
+
+    document.getElementById("")
+}
 EventSource.onKeyDown(Timer.start())
 EventSource.onKeyRelease(Timer.stop())
